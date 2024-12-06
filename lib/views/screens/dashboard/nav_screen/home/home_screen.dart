395287b0720b1f397616f837d16637d14b1f0dc3
Raka RG <rakaguntara10@gmail.com>
2024-12-06 20:23:05 +0700

    create header

--- lib/views/screens/dashboard/nav_screen/home/home_screen.dart
+++ lib/views/screens/dashboard/nav_screen/home/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:marketku/views/widgets/header.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("Home Screen"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [Header()],
+        ),
+      ),
     );
   }
 }
---
+++ lib/views/widgets/header.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:marketku/views/helpers/custom_colors.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.20,
+      child: Stack(
+        children: [
+          Image.asset(
+            './assets/icons/searchBanner.jpeg',
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          const Positioned(
+            left: 48,
+            top: 68,
+            child: SizedBox(
+              width: 250,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Search...",
+                    hintStyle: TextStyle(fontSize: 16, color: Colors.white),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                    prefixIcon: Icon(Icons.search),
+                    prefixIconColor: CustomColors.primary,
+                    suffixIcon: Icon(Icons.camera),
+                    suffixIconColor: CustomColors.primary,
+                    fillColor: Colors.white,
+                    filled: true,
+                    focusColor: Colors.grey),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 311,
+            top: 78,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {},
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/icons/bell.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 354,
+            top: 78,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {},
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/icons/message.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
